9a2a35aa890d88a124c21563807da2179a4a3919
Abdul Samad <132301326+samade747@users.noreply.github.com>
2024-07-20 14:09:00 +0500

    208

--- step10Tuples.ts
+++ step10Tuples.ts
@@ -14,22 +14,44 @@
 
 
 // accessing tuples elements 
-let person: [string, number] = ["maryam", 24]
-//  person mai index 0 par string and index 1 par number
-console.log(person[0]) 
-console.log(person[1])
+// let person: [string, number] = ["maryam", 24]
+// //  person mai index 0 par string and index 1 par number
+// console.log(person[0]) 
+// console.log(person[1])
 
 // using tuples in functions
 
-function printPerson(person: [string, number]) {
-    console.log(`my name is ${person[0]} and my age is ${person[1]}`)
+// function printPerson(person: [string, number]) {
+//     console.log(`my name is ${person[0]} and my age is ${person[1]}`)
 
-}
+// }
 
-printPerson(["maryam", 24])
+// printPerson(["maryam", 24])
 
 // funtion rturning a tuple
 
+// function getPerson(): [string, number]{
+//     return ["maryam", 24]
+// }
+
+// let person = getPerson()
+// console.log(person) 
 
 
+// Tuples with Optional Elements
+
+// let person: [string, number?, boolean?] 
+
+// person = ["maryam"] // correct
+// person = ["maryam", 24] // correct
+// person = ["maryam", 24, true] // correct
+// person = ["maryam", 24, true, false] // incorrect
+
+// tuples with rest Elements
+
+let person: [string, ...number[]]
+
+person = ["maryam"] // correct
+person = ["maryam", 24, 25, 26, 27, 28] // correct
+person = [25, "maryam"] // incorrect
 
