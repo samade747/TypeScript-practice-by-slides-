d6387f5d469b072a8ebff05cbdb3fb7d9323cfb8
Abdul Samad <132301326+samade747@users.noreply.github.com>
2024-07-22 02:05:47 +0500

    .

--- callbacksmainpart.ts
+++ callbacksmainpart.ts
@@ -14,15 +14,15 @@
 // 2 step
 
 
-// function processUserInput(callback: (name: string, lastName: string) => void) {
-//     let name = "samad"
-//     let lastName = "maryam"
-//     callback(name, lastName)
-// }
+function processUserInput(callback: (name: string, lastName: string) => void) {
+    let name = "samad"
+    let lastName = "maryam"
+    callback(name, lastName)
+}
 
-// processUserInput(function(name:string, lastName:string){
-//     console.log(`hello ${name} and welcome to ${lastName}`)
+processUserInput(function(name:string, lastName:string){
+    console.log(`hello ${name} and welcome to ${lastName}`)
 
-// })
+})
 
 
